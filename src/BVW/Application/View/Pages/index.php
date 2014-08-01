89ebdd830e3c9f73da73649001a08f4904265b76
Bruno Vaula Werneck <brunovaulawerneck@gmail.com>
2014-08-01 10:28:30 -0300

    Consertando marcação do twitter bootstrap na página inicial

--- src/BVW/Application/View/Pages/index.php
+++ src/BVW/Application/View/Pages/index.php
@@ -1,10 +1,14 @@
 <div class="container">
     <div class="row">
-        <div class="page-header">
-            <h3>Cadastro de Clientes</h3>
+        <div class="col-xs-12">
+            <div class="page-header">
+                <h3>Cadastro de Clientes</h3>
+            </div>
         </div>
     </div>
     <div class="row">
-        <button type="button" class="btn btn-primary" role='ClientsList'><i class="fa fa-users"></i> Lista de Clientes</button><br />        
+        <div class="col-xs-12">
+            <button type="button" class="btn btn-primary" role='ClientsList'><i class="fa fa-users"></i> Lista de Clientes</button><br />        
+        </div>
     </div>
 </div>
